1bb5af5a51983c0e21bd69e2a96db25741f43acc
Victor R. Silva <victorr.silva005@gmail.com>
2024-04-21 13:23:54 -0300

    Finished Linq logic

--- ConsoleApp32/Program.cs
+++ ConsoleApp32/Program.cs
@@ -9,6 +9,9 @@ namespace ConsoleApp32
             Console.Write("Enter full file path: ");
             string path = Console.ReadLine();
 
+            Console.Write("Enter salary: ");
+            double avgSalary = double.Parse(Console.ReadLine());
+
             List<Employee> employees = new List<Employee>();
 
             using(StreamReader reader = File.OpenText(path))
@@ -23,7 +26,17 @@ namespace ConsoleApp32
                     employees.Add(new Employee(name, email,salary));
                 }
 
+                var avg = employees.Where(p => p.Salary > avgSalary).OrderBy(p => p.Email).Select(p => p.Email);
 
+                Console.WriteLine("Email of people whose salary is greater than " + avgSalary);
+
+                foreach (string employee in avg) {
+                    Console.WriteLine(employee);
+                }
+
+                var sum = employees.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+
+                Console.WriteLine("Sum of salary of people whose name stars with 'M': " + sum);
             }
         }
     }
