e4da2a2539f880576d10f811896881abe599919f
Victor R. Silva <victorr.silva005@gmail.com>
2024-04-21 12:00:22 -0300

    Created file reading program

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+
+            using(StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] strings = reader.ReadLine().Split(',');
+                    string name = strings[0];
+                    string email = strings[1];
+                    double salary = Double.Parse(strings[2]);
+
+                    employees.Add(new Employee(name, email,salary));
+                }
+
+
+            }
+        }
+    }
+}
